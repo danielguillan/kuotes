c971033165bf83e55f607f73ddc2a46e47173005
Daniel Guillan <daniel.guillan@gmail.com>
2012-10-13 13:57:41 +0200

    Kuotes now have their own page and url

--- config.rb
+++ config.rb
@@ -10,6 +10,15 @@ set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
 
+data.kuotes.each_with_index do |k, i|
+  # Generate kuote page
+  page "/kuote/#{i}.html", :proxy => "/kuote.html" do
+    @kuote = k
+  end
+end
+
+#puts authors
+
 # Build-specific configuration
 configure :build do
   activate :minify_css
